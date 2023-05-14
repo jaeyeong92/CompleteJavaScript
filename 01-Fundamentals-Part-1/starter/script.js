19f8b5aaec5dadf18afa381172abb398063b3b20
jaeyeong92 <jjy4041@gmail.com>
2023-05-14 17:39:26 +0900

    Complete JS Learning
    20230514 Commit

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,26 @@
+let js = 'amazing';
+if(js === 'amazing') console.log('JavaScript is FUN !');
+
+let num = 40 + 8 + 23 - 10;
+console.log(num);
+
+let PI = 3.1415;
+
+
+// let const var
+let age = 30;
+age = 31;     // 재할당 가능
+
+// let age = 32; // 재선언 불가능
+
+
+
+const birthYear = 1992;
+// birthYear = 1995;       // 실행 시, Error -> 재할당 불가능
+
+var dog = "nuri";
+
+var dog = "yani";   // 재선언 자체가 가능하다
+
+console.log(dog);
+
